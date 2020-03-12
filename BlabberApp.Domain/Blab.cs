67b38c50d98e68a8b9f96a6abd90e46090f16d11
Aubrey Sparrow <aubreysparrow@mail.weber.edu>
2020-03-11 18:17:09 -0600

    streamlined code in Blab.cs

--- BlabberApp.Domain/Blab.cs
+++ BlabberApp.Domain/Blab.cs
@@ -4,24 +4,14 @@ namespace BlabberApp.Domain
 {
     public class Blab : BaseDatum
     {
-        private string _message;
-        public string Message
-        {
-            get { return this._message; }
-            set { this._message = value; }
-        }
-        private DateTime _dttm;
-        public DateTime DTTM
-        {
-            get { return this._dttm; }
-            set { this._dttm = value; }
-        } //DTTM = DateTime
-        private string _userId; //probably will change
-        public string UserID
-        {
-            get { return this._userId; }
-            set { this._userId = value; }
-        }
+       public DateTime DTTM {get; set;}
+       public string Message{get; set;}
+       public string UserID{get; set;}
+
+       public Blab()
+       {
+           DTTM = DateTime.Now;
+       }
 
     }
 }
